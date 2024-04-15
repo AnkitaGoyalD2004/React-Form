a0ce95463c2f6221ba63503eb3463677d17428a7
AnkitaGoyalD2004 <goyalankita0325@gmail.com>
2024-04-15 15:31:40 +0530

    setup basics

--- src/Register.jsx
+++ src/Register.jsx
@@ -33,6 +33,22 @@ const Register = () => {
     useRef.current.focus();
   }, []);
 
+  useEffect(() => {
+    const result = USER_REGEX.test(user);
+    console.log(result);
+    console.log(user);
+    setValidName(result);
+  }, [user]);
+
+  useEffect(() => {
+    const result = PWD_REGEX.test(pwd);
+    console.log(result);
+    console.log(pwd);
+    setValidPwd(result);
+    const match = pwd === matchPwd;
+    setValidMatch(match);
+  }, [pwd, matchPwd]);
+
   return <div></div>;
 };
 
